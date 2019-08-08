f4af7919e633904f08e03db868c3e7e273685c49
moluyaonie <5126054+moluyaole@user.noreply.gitee.com>
2019-08-08 21:47:23 +0800

    '自定义配置文件'

--- src/main.js
+++ src/main.js
@@ -7,4 +7,5 @@ Vue.config.productionTip = false
 new Vue({
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') //.$mount('#app')和 el:'#app" 一样,,只是多了一层内部判断
+//3.0 以上的vue安装cli后都会默认安装 vue UI  ==>  vue ui 可以可视化,创建和导入项目
---
+++ vue.config.js
@@ -0,0 +1,2 @@
+//这个文件 是自己配置webpack 的配置,,它最终会和隐藏的webpack 进行合并
+module.exports={}
